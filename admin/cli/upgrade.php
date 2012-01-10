67e9c4b98d3e62f3588cc1a031438f8dfb75412c
Barbara Ramiro <barbara@moodle.com>
2012-01-10 16:22:10 +0800

    Revert "Adding a new comment"
    This reverts commit ce3b132065ed7419fa2e744c593ef48643a6af0f.

--- admin/cli/upgrade.php
+++ admin/cli/upgrade.php
@@ -29,9 +29,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
-// this is a new comment to test
-
 define('CLI_SCRIPT', true);
 
 require(dirname(dirname(dirname(__FILE__))).'/config.php');
