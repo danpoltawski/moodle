42a017088772b9d64392fab269893f5a64f52e74
Sam Hemelryk <sam@moodle.com>
2012-04-17 13:05:37 +1200

    MDL-31270 tool_assignmentupgrade: Fixed up camel casing in strings

--- admin/tool/assignmentupgrade/lang/en/tool_assignmentupgrade.php
+++ admin/tool/assignmentupgrade/lang/en/tool_assignmentupgrade.php
@@ -1,20 +1,20 @@
 <?php
 
-$string['pluginname'] = 'Assignment Upgrade Helper';
+$string['pluginname'] = 'Assignment upgrade helper';
 $string['listnotupgraded'] = 'List assignments that have not been upgraded';
 $string['listnotupgraded_desc'] = 'You can upgrade individual assignments from here';
-$string['notupgradedtitle'] = 'Assignments Not Upgraded';
-$string['notupgradedintro'] = 'This page lists the assignments created with an older version of Moodle that have not been upgraded to the new assignment format in Moodle 2.3. Not all assignments can be upgraded - if they were created with a custom assignment subtype, then that subtype will need to be upgraded to the new assignment plugin format in order to complete the upgrade.';
+$string['notupgradedtitle'] = 'Assignments not upgraded';
+$string['notupgradedintro'] = 'This page lists the assignments created with an older version of Moodle that have not been upgraded to the new assignment module in Moodle 2.3. Not all assignments can be upgraded - if they were created with a custom assignment subtype, then that subtype will need to be upgraded to the new assignment plugin format in order to complete the upgrade.';
 $string['assignmentid'] = 'Assignment ID';
-$string['assignmenttype'] = 'Assignment Type';
+$string['assignmenttype'] = 'Assignment type';
 $string['submissions'] = 'Submissions';
-$string['backtoindex'] = 'Back to Index';
+$string['backtoindex'] = 'Back to index';
 $string['upgradable'] = 'Upgradable';
 $string['notsupported'] = '';
 $string['supported'] = 'Upgrade';
-$string['upgradesingle'] = 'Upgrade Single Assignment';
+$string['upgradesingle'] = 'Upgrade single assignment';
 $string['areyousure'] = 'Are you sure?';
 $string['areyousuremessage'] = 'Are you sure you want to upgrade the assignment "{$a->name}"?';
-$string['conversioncomplete'] = 'Assignment Converted';
-$string['conversionfailed'] = 'The assignment conversion was not successful. The log from the upgrade was: <br/>{$a}';
+$string['conversioncomplete'] = 'Assignment converted';
+$string['conversionfailed'] = 'The assignment conversion was not successful. The log from the upgrade was: <br />{$a}';
 $string['viewcourse'] = 'View the course with the converted assignment';
