d98c96e8190bd7c542b4e7f62bb108038720d0c3
Dan Poltawski <dan@moodle.com>
2015-05-14 12:50:51 +0100

    MDL-49297 qtype_shortanswer: fix whitespace error

--- question/type/shortanswer/renderer.php
+++ question/type/shortanswer/renderer.php
@@ -43,7 +43,7 @@ class qtype_shortanswer_renderer extends qtype_renderer {
         $inputname = $qa->get_qt_field_name('answer');
         $inputattributes = array(
             'type' => 'text',
-            'spellcheck' =>false,
+            'spellcheck' => false,
             'name' => $inputname,
             'value' => $currentanswer,
             'id' => $inputname,
