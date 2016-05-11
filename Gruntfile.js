160f933808993795d646ace2d6c940fce4c4f2a9
Dan Poltawski <dan@moodle.com>
2016-05-11 11:38:46 +0100

    MDL-52127 js: basic YUI checking with eslint
    Because we don't have the entire YUI rollup, we can't apply some of the
    rules safely, so we ignore some rules.
    
    At this point I am not 100% certain this configuration is safe enough,
    although its a good sign that there are no errors on our entire yui
    codebase.

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,6 +77,12 @@ module.exports = function(grunt) {
             options: { quiet: !grunt.option('eslint-warnings') },
             // Check AMD files with standard config
             amd: { src: amdSrc },
+            // Some rules disabled for YUI config, because we don't do rollup magic, so its able to be accurate
+            // about undefined items.
+            yui: {
+               src: ['**/yui/src/**/*.js'],
+               options: {globals: ['Y', 'YUI'], rules: {'no-undef': 'off', 'no-unused-vars': 'off', 'no-empty': 'off'} }
+            }
         },
         uglify: {
             amd: {
@@ -252,8 +258,9 @@ module.exports = function(grunt) {
 
     // Register JS tasks.
     grunt.registerTask('shifter', 'Run Shifter against the current directory', tasks.shifter);
+    grunt.registerTask('yui', ['eslint:yui', 'shifter']);
     grunt.registerTask('amd', ['eslint:amd', 'jshint', 'uglify']);
-    grunt.registerTask('js', ['amd', 'shifter']);
+    grunt.registerTask('js', ['amd', 'yui']);
 
     // Register CSS taks.
     grunt.registerTask('css', ['less:bootstrapbase']);
