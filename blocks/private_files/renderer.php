3ca5ac3f982547c496692a1a404a0ee8a06c77b4
Dan Poltawski <dan@moodle.com>
2016-12-30 13:58:44 +0000

    MDL-32736 block_privatefiles: migrate away from yui2 treeview

--- blocks/private_files/renderer.php
+++ blocks/private_files/renderer.php
@@ -34,56 +34,90 @@ class block_private_files_renderer extends plugin_renderer_base {
         return $this->render(new private_files_tree);
     }
 
-    public function render_private_files_tree(private_files_tree $tree) {
-        $module = array('name'=>'block_private_files', 'fullpath'=>'/blocks/private_files/module.js', 'requires'=>array('yui2-treeview'));
-        if (empty($tree->dir['subdirs']) && empty($tree->dir['files'])) {
-            $html = $this->output->box(get_string('nofilesavailable', 'repository'));
-        } else {
-            $htmlid = 'private_files_tree_'.uniqid();
-            $this->page->requires->js_init_call('M.block_private_files.init_tree', array(false, $htmlid));
-            $html = '<div id="'.$htmlid.'">';
-            $html .= $this->htmllize_tree($tree, $tree->dir);
-            $html .= '</div>';
-        }
-
-        return $html;
-    }
-
     /**
-     * Internal function - creates htmls structure suitable for YUI tree.
+     * Renders a private files tree.
+     *
+     * @param private_files_tree $tree
+     * @return string The html rendered.
      */
-    protected function htmllize_tree($tree, $dir) {
-        global $CFG;
-        $yuiconfig = array();
-        $yuiconfig['type'] = 'html';
-
-        if (empty($dir['subdirs']) and empty($dir['files'])) {
-            return '';
-        }
-        $result = '<ul>';
-        foreach ($dir['subdirs'] as $subdir) {
-            $image = $this->output->pix_icon(file_folder_icon(), $subdir['dirname'], 'moodle', array('class'=>'icon'));
-            $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.$image.s($subdir['dirname']).'</div> '.$this->htmllize_tree($tree, $subdir).'</li>';
-        }
-        foreach ($dir['files'] as $file) {
-            $url = file_encode_url("$CFG->wwwroot/pluginfile.php", '/'.$tree->context->id.'/user/private'.$file->get_filepath().$file->get_filename(), true);
-            $filename = $file->get_filename();
-            $image = $this->output->pix_icon(file_file_icon($file), $filename, 'moodle', array('class'=>'icon'));
-            $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.html_writer::link($url, $image.$filename).'</div></li>';
-        }
-        $result .= '</ul>';
-
-        return $result;
+    public function render_private_files_tree(private_files_tree $tree) {
+        $data = $tree->export_for_template($this);
+        return $this->render_from_template('core/filetree', $data);
     }
 }
 
-class private_files_tree implements renderable {
+/**
+ * Private files tree renderable.
+ *
+ * @package    block_private_files
+ * @copyright  2010 Dongsheng Cai <dongsheng@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class private_files_tree implements renderable, templatable {
     public $context;
     public $dir;
+    /**
+     * Constructor.
+     */
     public function __construct() {
         global $USER;
         $this->context = context_user::instance($USER->id);
         $fs = get_file_storage();
         $this->dir = $fs->get_area_tree($this->context->id, 'user', 'private', 0);
+    }
+
+    /**
+     * Export the data.
+     *
+     * @param renderer_base $output
+     * @return array data required by template
+     */
+    public function export_for_template(renderer_base $output) {
+        return [
+            'foldersexpanded' => false,
+            'files' => $this->prepare_dir_for_template($this->dir, $output)
+        ];
+    }
+
+    /**
+     * Generates the tree structure required by core/filetree template for a dir
+     *
+     * @param array $dir dir tree from $fs->get_area_tree()
+     * @param renderer_base $output
+     * @return array tree structure required by core/filetree template.
+     */
+    protected function prepare_dir_for_template($dir, $output) {
+        $files = [];
+        foreach ($dir['subdirs'] as $subdir) {
+            $dirfiles = $this->prepare_dir_for_template($subdir, $output);
+            $files[] = ['title' => $subdir['dirname'], 'isdir' => true, 'files' => $dirfiles];
+        }
+
+        foreach ($dir['files'] as $file) {
+            $files[] = $this->prepare_file_for_template($file, $output);
+        }
+        return $files;
+    }
+
+    /**
+     * Generates the structure required by core/filetree template for a file
+     *
+     * @param array $file from $fs->get_area_tree()
+     * @param renderable $page
+     * @return array
+     */
+    protected function prepare_file_for_template($file, $output) {
+        global $CFG;
+
+        $filename = $file->get_filename();
+        $path = "/{$this->context->id}/user/private{$file->get_filepath()}/$filename";
+
+        $data = [];
+        $data['url'] = file_encode_url("$CFG->wwwroot/pluginfile.php", $path, true);
+        $data['icon'] = $output->pix_icon(file_file_icon($file), $filename, 'moodle', array('class' => 'icon'));
+        $data['title'] = $filename;
+        $data['isdir'] = false;
+
+        return $data;
     }
 }
