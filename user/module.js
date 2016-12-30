f2b4ad0838355edfc0581f282a11fff36eb87d05
Dan Poltawski <dan@moodle.com>
2016-12-30 14:30:22 +0000

    MDL-32736 user: remove unused treeview

--- user/module.js
+++ user/module.js
@@ -39,20 +39,3 @@ M.core_user.init_participation = function(Y) {
         });
     }, '#checknone');
 };
-
-M.core_user.init_tree = function(Y, expand_all, htmlid) {
-    Y.use('yui2-treeview', function(Y) {
-        var tree = new Y.YUI2.widget.TreeView(htmlid);
-
-        tree.subscribe("clickEvent", function(node, event) {
-            // we want normal clicking which redirects to url
-            return false;
-        });
-
-        if (expand_all) {
-            tree.expandAll();
-        }
-
-        tree.render();
-    });
-};
